b5f90141ce5ea00bd85e7f00aa79caeae5cd2a1c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-04-13 09:45:19 +0530

    CURD application

--- JDBCProject/src/CURD.java
+++ JDBCProject/src/CURD.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.MissingFormatArgumentException;
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.sql.SQLException;
 
@@ -53,6 +55,7 @@ class DatabaseCreated {
 // New Class
 class Inserting extends basicInfo {
     public void insertValue() {
+        Scanner sc=new Scanner(System.in);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");//Driver Location
 
@@ -64,7 +67,20 @@ class Inserting extends basicInfo {
         try {
             Connection conn = DriverManager.getConnection(url, username, password);
             Statement stat = conn.createStatement();
-            
+            System.out.println("Enter the Roll Number of the Student\nand Press STOP for stop Entering Data");
+            while(true){
+                String str=sc.nextLine();
+                if(str.equalsIgnoreCase("STOP")){
+                    return;
+                }
+                try{
+
+                }
+                catch(MissingFormatArgumentException e){
+                    e.printStackTrace();
+                }
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "ERROR:", e.getMessage(), JOptionPane.ERROR_MESSAGE);
